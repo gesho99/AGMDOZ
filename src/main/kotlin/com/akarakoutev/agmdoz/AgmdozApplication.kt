40cd6e56fe32f7679903e56b6e1ada415021dba1
Alberto Karakoutev <alberto.karakoutev@smule.com>
2022-11-08 19:30:51 +0200

    #1 Add initial backend project skeleton

---
+++ src/main/kotlin/com/akarakoutev/agmdoz/AgmdozApplication.kt
@@ -0,0 +1,11 @@
+package com.akarakoutev.agmdoz
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AgmdozApplication
+
+fun main(args: Array<String>) {
+	runApplication<AgmdozApplication>(*args)
+}
---
+++ src/test/kotlin/com/akarakoutev/agmdoz/AgmdozApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.akarakoutev.agmdoz
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AgmdozApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
